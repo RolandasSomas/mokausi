7f09b4c96ca34f79535838a3456578dcfe9b44c7
Rolandas <Srolkis@gmail.com>
2024-04-02 21:51:16 +0300

    nauja paskaita

--- 03-28/pratimas_4.py
+++ 03-28/pratimas_4.py
@@ -8,7 +8,9 @@ tekstas = ('Pamėginkite suprasti kas čia vyksta. Patarimas: judėti nuo '
            ' sujungti detalę po detalės kol bus viskas aišku')
 
 lauzytas_tekstas= tekstas.split()
-
+suma=0
 for i in lauzytas_tekstas:
     if len(i) > 5:
+        suma += 1
         print('zodis turi daugiau nei 5 simbolius: ',i)
+print('suma',suma)
---
+++ 04-02/uzduotis5.py
@@ -0,0 +1,41 @@
+'''
+Parašykite programą, apibrėžiančią funkciją extract_email_addresses(),
+kuri priima tekstą kaip įvestį ir iš teksto ištraukia visus el. pašto adresus
+'''
+# import re
+# def extract_emails(text):
+#         # Define the regular expression pattern for an email address
+#         pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+#
+#         # Use the findall() function to extract all email addresses from the text
+#         emails = re.findall(pattern, text)
+#
+#         return emails
+# text = "Contact us at email@example.com or support@example.org for assistance."
+# emails = extract_emails(text)
+# print(emails)
+
+def extract_email_addresses():
+    enter = input("Enter text containing Email addresses: ")
+    emails = []
+    for email in enter.split():
+        if "@" in email and "." in email: emails.append(email)
+        return emails
+
+emails = extract_email_addresses()
+print(emails)
+
+
+
+#
+# tekstas=('Contact us at email@example.com or support@example.org for assistance')
+# kapotas_tekstas = tekstas.split()
+# def extract_email_addresses(tekstas:str) -> str:
+#         if kapotas_tekstas == '@' and kapotas_tekstas == '.':
+#                 return
+#
+# a = kapotas_tekstas
+# print(a)
+
+
+
